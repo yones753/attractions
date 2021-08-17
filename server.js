3bc757cd45f07b195a870b0150e2c14a03e87707
Yones <deraweyones@gmail.com>
2021-08-17 22:38:50 +0300

    update server

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ const app = express()
 const PORT = 8080
 
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/attractions", { useNewUrlParser: true, useUnifiedTopology: true })  
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/', api)
 
-app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 app.listen( process.env.PORT || PORT, function () {
     console.log(`Server up and running on port ${PORT}`)
 })
