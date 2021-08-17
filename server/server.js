c842e1a497d4e12fc1e868e1cdbbaaddbe418045
Yones <deraweyones@gmail.com>
2021-08-17 21:23:34 +0300

    update url in font

--- server/server.js
+++ server/server.js
@@ -3,16 +3,23 @@ const mongoose = require('mongoose')
 const api = require('../server/routes/api')
 const app = express()
 const PORT = 8080
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+
 
 //process.env.MONGODB_URI || 
 mongoose.connect("mongodb://localhost/attractions", { useNewUrlParser: true, useUnifiedTopology: true })  
-
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use('/', api)
 
 
 // process.env.URL
-app.use('/', api)
+
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.listen( PORT, function () {
     console.log(`Server up and running on port ${PORT}`)
 })
--- src/components/constant.js
+++ src/components/constant.js
@@ -1,3 +1,3 @@
 export const logoYad2 = "https://assets.yad2.co.il/yad2site/y2assets/images/header/yad2Logo.png";
 export const homeImg ="https://images.pexels.com/photos/1078850/pexels-photo-1078850.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500";
-export const url = "http://localhost:8080/attraction"; 
+export const url = "/attraction"; 
