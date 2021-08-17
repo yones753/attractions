795204ff257d2cedff4684d7d85fee41f86e2a8c
Yones <deraweyones@gmail.com>
2021-08-17 23:21:16 +0300

    change server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
 const api = require('./server/routes/api')
@@ -7,8 +8,12 @@ const PORT = 8080
 mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/attractions", { useNewUrlParser: true, useUnifiedTopology: true })  
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/', api)
 
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen( process.env.PORT || PORT, function () {
     console.log(`Server up and running on port ${PORT}`)
